4d7c22cebe843392b8b016c069f444a5fa8c9b5a
venkata-naveen-varma <venkata2naveen@gmail.com>
2024-12-19 13:47:17 -0500

    update skills

--- src/components/sections/Skills.jsx
+++ src/components/sections/Skills.jsx
@@ -127,8 +127,7 @@ const Skills = () => {
             marginBottom: "40px",
           }}
         >
-          Here are some of my skills on which I have been working on for the
-          past 3 years.
+          Here are some of the skills I have acquired through hands-on experience.
         </Desc>
 
         <SkillsContainer>
